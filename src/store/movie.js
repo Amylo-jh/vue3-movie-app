0bd10b4d678eeaea1c1fd8f7519f424289b7a843
Amylo <amylose1110@gmail.com>
2024-02-04 15:30:33 +0900

    Part 8 - Ch 04 - 12. 영화 검색 추가 요청

--- src/store/movie.js
+++ src/store/movie.js
@@ -19,7 +19,7 @@ export default {
     }
   },
   actions: {
-    async searchMovies({ commit }, payload) {
+    async searchMovies({ state, commit }, payload) {
       const { title, type, number, year } = payload
       const OMDB_API_KEY = '1451733a'
 
@@ -28,6 +28,27 @@ export default {
       commit('updateState', {
         movies: Search
       })
+      console.log(totalResults)
+      console.log(typeof totalResults)
+
+      const total = parseInt(totalResults, 10)
+      const pageLength = Math.ceil(total / 10)
+
+      if(pageLength > 1)
+      {
+        for(let page = 2; page <= pageLength; page++)
+        {
+          if(page > (number / 10))
+          {
+            break
+          }
+          const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)
+          const { Search } = res.data
+          commit('updateState', {
+            movies: [...state.movies, ...Search]
+          })
+        }
+      }
     }
   }
 }
