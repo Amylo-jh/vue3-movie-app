cd6f0cd2cca51958c5b0ad7a953ad990d1e9030a
Amylo <amylose1110@gmail.com>
2024-02-10 18:30:36 +0900

    Part 8 - Ch 04 - 22. Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer>
+    <Logo />
+    <a
+      href="https://github.com/Amylo-jh"
+      target="_blank">
+      (c){{ new Date().getFullYear() }} Amylo
+    </a>
+  </footer>
+</template>
+
+<script>
+import Logo from '~/components/Logo'
+export default {
+    components: {
+        Logo
+    }
+}
+</script>
+
+<style>
+footer {
+    padding: 70px 0;
+    text-align: center;
+    opacity:.3;
+    .logo {
+        display: block;
+        margin-bottom: 4px;
+    }
+}
+</style>
